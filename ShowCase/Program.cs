dc212be3c5a6086a261846bd092c3f2abec6f672
Daniel <idaniiel16@gmail.com>
2022-03-22 23:53:14 -0600

    Proyecto que crea una biblioteca de clases

---
+++ ShowCase/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UtilityLibraries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 0;
+            do
+            {
+                if (row == 0 || row >= 25)
+                    row = RecetConsole(row);
+
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) break;
+                Console.WriteLine($"Input: {input}");
+                Console.WriteLine("Begins with uppercase? " + $"{(input.StartsWithUpper()? "Yes":"No")}");
+                Console.WriteLine();
+                row += 4;
+            } while (true);
+            return; 
+        }
+
+        static int RecetConsole(int row)
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine} ");
+            return row = 3;
+        }
+    }
+}
---
+++ StringLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9de26a7-aa07-42f9-8c34-b93343387c77")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
